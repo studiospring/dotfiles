0a5f70f14f6758a82439013e4a7a0729c253b4f5
Sean <lettersforsean@yahoo.co.jp>
2016-12-28 14:03:17 +1100

    Minor changes to fzf config.

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,18 +1,18 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "/home/sean/.fzf/bin" ]]; then
+if [[ ! "$PATH" == */home/sean/.fzf/bin* ]]; then
   export PATH="$PATH:/home/sean/.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "/home/sean/.fzf/man" && -d "/home/sean/.fzf/man" ]]; then
+if [[ ! "$MANPATH" == */home/sean/.fzf/man* && -d "/home/sean/.fzf/man" ]]; then
   export MANPATH="$MANPATH:/home/sean/.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "/home/sean/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/sean/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
